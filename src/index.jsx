24d421cb018ad41129fe3650613a3f3ddefeb16d
bluevisionai <nepfumbada.mashudu@gmail.com>
2023-03-30 21:27:10 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,16 +2,17 @@ import { CssBaseline } from '@mui/material';
 import { StyledEngineProvider } from '@mui/styled-engine';
 import 'perfect-scrollbar/css/perfect-scrollbar.css';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 import App from './app/App';
 import * as serviceWorker from './serviceWorker';
-
+// HashRouter
 ReactDOM.render(
   <StyledEngineProvider injectFirst>
-    <BrowserRouter basename='/BinPack'>
+    <HashRouter >
       <CssBaseline />
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StyledEngineProvider>,
   document.getElementById('root')
 );
