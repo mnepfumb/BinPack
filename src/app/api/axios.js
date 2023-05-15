6f5e1af87f6c5d11febde0f42a0a8059518f1a44
bluevisionai <nepfumbada.mashudu@gmail.com>
2023-05-15 18:12:20 +0200

    Update axios.js

--- src/app/api/axios.js
+++ src/app/api/axios.js
@@ -1,4 +1,15 @@
 import axios from 'axios';
+const { REACT_APP_API_BASE_URL } = process.env;
+
+console.log(REACT_APP_API_BASE_URL)
+
+const BASE_URL = process.env.REACT_APP_API_BASE_URL;
+// const BASE_URL = 'http://localhost:3500';
+// const BASE_URL = 'https://binpackapi.azurewebsites.net';
+// https://binpackapi.azurewebsites.net
+console.log(BASE_URL)
+
+console.log(`${process.env.REACT_APP_API_BASE_URL}`)
 
 export default axios.create({
     
