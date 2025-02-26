ba075da134b7734d820a5068df8ef7a7b60f61f6
Mashudu <nepfumbada.mashudu@gmail.com>
2025-02-26 20:43:24 +0200

    Update axios.js

--- src/app/api/axios.js
+++ src/app/api/axios.js
@@ -4,10 +4,10 @@ const { REACT_APP_API_BASE_URL } = process.env;
 console.log(REACT_APP_API_BASE_URL)
 
 // const BASE_URL = process.env.REACT_APP_API_BASE_URL;
-// const BASE_URL = 'http://localhost:3500';
+const BASE_URL = 'http://localhost:3500';
 // const BASE_URL = 'https://binpackdevapi-fma7ebfdhvhpghd0.southafricanorth-01.azurewebsites.net';
 // const BASE_URL = 'https://binpackapidev-fshva9cefwcwhegk.southafricanorth-01.azurewebsites.net';
-const BASE_URL = 'https://binpack-api.onrender.com';
+// const BASE_URL = 'https://binpack-api.onrender.com';
 // https://binpackapi.azurewebsites.net
 // console.log(BASE_URL)
 
